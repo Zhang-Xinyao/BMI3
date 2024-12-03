74d6ccdbc07b12cf98f3c252648ed4e9d6cd0046
Zhang-Xinyao <127190752+Zhang-Xinyao@users.noreply.github.com>
2024-12-03 21:39:54 +0800

    Update data_prepare.py

--- data_prepare.py
+++ data_prepare.py
@@ -3,6 +3,7 @@ import os
 
 print("This is our program finding gRNAs")
 print("You are currently in the directory:", os.getcwd())
+# /Your_path_to/bat1K
 target_dir = input("Please enter the full path to the folder where your data is placed: ")
 
 
@@ -13,7 +14,7 @@ else:
     print("The directory you entered does not exist. Please restart the program and try again.")
     exit()
 
-
+# the phylogeny tree of our species are stored in the following dictionary
 clusters={"cluster1":["HLhipCyc2","HLaseSto2","HLhipLar2"],
           "cluster2":["HLrhiAff2","HLrhiSin3"],
           "cluster3":["HLrhiSed2","HLrhiLuc4","HLrhiTri2"],
@@ -34,6 +35,7 @@ cluster_neighbors = {
     "cluster8": []
 }
 
+# we use HLrhiTri2 as example
 print("Here are the bat species you can choose:")
 species = []
 for key,val in clusters.items():
@@ -44,22 +46,29 @@ species_name=input("Please choose a species: ")
 if species_name not in species:
     sys.exit("Can't find the species. Please try again.")
 
-def get_cluster_members(input_element):
+def get_members(input_element):
+    neighbor_cluster=0
     for cluster_name, elements in clusters.items():
         if input_element in elements:
             if len(elements) > 1:
-                return [e for e in elements if e != input_element]
+                return [e for e in elements if e != input_element], neighbor_cluster
             else:
+                # if there is no other members in its own cluster, we will search its neighboring cluster
                 neighbors = cluster_neighbors.get(cluster_name, [])
                 members = []
                 for neighbor in neighbors:
+                    neighbor_cluster += 1
                     members.extend(clusters.get(neighbor, []))
-                return members
-    return None
+                return members,neighbor_cluster
+    return [], 0
 
-similar_member = get_cluster_members(species_name)
+similar_member, neighbors = get_members(species_name)
 if similar_member:
+    if neighbors > 0:
+        print("There is no members in its own cluster. The members in its neighboring clusters will be listed.")
     print(f"Similar members are shown as follows: {', '.join(similar_member)}")
+    # example
+    # Similar members are shown as follows: HLrhiSed2, HLrhiLuc4
 else:
     print("No similar members found. You can choose other species.")
     sys.exit()
@@ -67,14 +76,20 @@ else:
 
 target_path = os.path.join(target_dir, species_name)
 
+# read the fasta and gtf file
 genome_fasta = os.path.join(target_path, f"{species_name}.fa")
 if os.path.exists(genome_fasta):
     print("Genome fasta file already exists")
 else:
-    print("Genome fasta file does not exist")
+    sys.exit("Genome fasta file does not exist")
 
-genome_gtf = os.path.join(target_path, f"{species_name}_annotation.sorted.gtf")
-if os.path.exists(genome_gtf):
+genome_gtf1 = os.path.join(target_path, f"{species_name}_annotation.sorted.gtf")
+genome_gtf2 = os.path.join(target_path, f"{species_name}.gtf")
+if os.path.exists(genome_gtf1):
     print("Genome gtf file already exists")
+    genome_gtf = genome_gtf1
+elif os.path.exists(genome_gtf2):
+    print("Genome gtf file already exists")
+    genome_gtf = genome_gtf2
 else:
-    print("Genome gtf file does not exist")
+    sys.exit("Genome gtf file does not exist")
